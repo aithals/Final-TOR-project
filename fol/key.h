73914851d485f002d73027f88795bf37ba7d9fdc
Shashanka <aithals@usc.edu>
2015-01-17 06:09:23 -0500

    2nd commit

--- fol/key.h
+++
@@ -1,9 +0,0 @@
-#ifndef KEY_H_   /* Include guard */
-#define KEY_H_
-extern unsigned char myyykey[16];
-extern unsigned char myyykey8[6][16];
-extern unsigned short inocmid;
-extern struct sockaddr_in pvaddr;
-extern struct incmlist *listtt;
-extern int pktsent;
-#endif
